5fb44a4e7d89610b6d63d05b211eafa5e2ba207d
mfl4 <mhdfarhanl6267@gmail.com>
2024-12-13 07:20:02 +0700

    (config): delete the deprecated config

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const config = {
   HOST: process.env.HOST || 'localhost',
   PORT: process.env.PORT || 3000,
-  MODEL_URL: process.env.MODEL_URL,
 };
 
 module.exports = config;
